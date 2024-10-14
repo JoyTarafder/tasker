46454bc3b26cc64a10889b6f28564036dc8491a2
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-10-14 22:43:32 +0600

    TaskBoard done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-
-
+import TaskBoard from "./components/Task/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
-      <HeroSection/>
+      <HeroSection />
+      <TaskBoard/>
       <Footer />
     </>
   );
