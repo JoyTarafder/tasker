dedfd4e457e1054334f23795fc050c8229e3dc27
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-10-15 22:58:05 +0600

    Add Task taskBoard done

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,34 +1,35 @@
-/* eslint-disable no-unused-vars */
 import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
-import AddTaskModal from "./AddTaskModal";
 
 export default function TaskBoard() {
   const defTask = {
-    'id':crypto.randomUUID(),
-    'title': 'Task LWS',
-    'description': 'Lorem',
-    'tags': ['py', 'js'],
-    'priority': 'High',
-    'isFavorite': false
-  }
-   
-    const [tasks, setTasks] = useState([defTask]);
-    const [showAddModal, setShowAddModal] = useState(false);
+    id: crypto.randomUUID(),
+    title: "Task LWS",
+    description: "Lorem",
+    tags: ["py", "js"],
+    priority: "High",
+    isFavorite: false,
+  };
 
-    function handleAddTask(){
-      console.log("hello")
-    }
+  const [tasks, setTasks] = useState([defTask]);
+  const [showAddModal, setShowAddModal] = useState(false);
+
+  function handleAddTask(newTask) {
+    console.log(newTask);
+    setTasks([...tasks, newTask]);
+    setShowAddModal(false);
+  }
   return (
     <section className="mb-20" id="tasks">
-      {showAddModal && <AddTaskModal/>}
+      {showAddModal && <AddTaskModal onSave={handleAddTask} />}
       <div className="container">
         <SearchTask />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction onAddClick={() => setShowAddModal(true)} />
-          <TaskList tasks={tasks}/>
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
