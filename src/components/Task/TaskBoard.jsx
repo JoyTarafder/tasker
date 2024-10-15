3cc763bdc14b80b7817c4bf37e3f1b2bd5ff265c
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-10-15 21:30:27 +0600

    Task Modal Added

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,25 +1,33 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 export default function TaskBoard() {
   const defTask = {
     'id':crypto.randomUUID(),
-    'title': 'Task Title',
+    'title': 'Task LWS',
     'description': 'Lorem',
     'tags': ['py', 'js'],
     'priority': 'High',
-    'isFevorite': true
+    'isFavorite': false
   }
-    // eslint-disable-next-line no-unused-vars
+   
     const [tasks, setTasks] = useState([defTask]);
+    const [showAddModal, setShowAddModal] = useState(false);
+
+    function handleAddTask(){
+      console.log("hello")
+    }
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal/>}
       <div className="container">
         <SearchTask />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction />
+          <TaskAction onAddClick={() => setShowAddModal(true)} />
           <TaskList tasks={tasks}/>
         </div>
       </div>
